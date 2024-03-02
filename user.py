d73ef4326b3d25276b02511656bf2ce9d8eb5d90
ams4222 <ams4222@g.rit.edu>
2024-03-02 15:13:25 -0500

    weird camera errors

--- user.py
+++ user.py
@@ -22,35 +22,23 @@ def open_settings():
     os.popen("start ms-settings:")
 
 
-video = cv2.VideoCapture(0)
-detector = HandDetector(detectionCon=0.8, maxHands=1)
 
 def main():
-    #video = cv2.VideoCapture(0)
-    while True:
-        #detector = HandDetector(detectionCon=0.8, maxHands=1)
-        #video = cv2.VideoCapture(0)
-    
-        video.set(3, 640)
-        video.set(4, 480)
+    #currently_running = True
+  
+    while(True): 
+        video = cv2.VideoCapture(0)
+        detector = HandDetector(detectionCon=0.8, maxHands=1)
+        ret, frame = video.read() 
+  
+        cv2.imshow('Hi, user!', frame) 
 
-        currently_running = True
-
-        #while not currently_running:
-            #add a goodbye message?
-            #video.release()
-            #cv2.destroyAllWindows()
-            #sys.exit()
-
-        ret, frame = video.read()
         hands, img = detector.findHands(frame)
 
-        imgScaled = cv2.resize(img, (0, 0), None, 0.533, 0.533)
+        #video.release() 
 
-        cv2.rectangle(img, (0, 480), (300, 425), (0, 0, 0), -2)
-        cv2.rectangle(img, (640, 480), (300, 425), (0, 0, 0), -2)
-
-        cv2.imshow("Camera", imgScaled)
+        # cv2.rectangle(img, (0, 480), (300, 425), (0, 0, 0), -2)
+        # cv2.rectangle(img, (640, 480), (300, 425), (0, 0, 0), -2)
 
         if hands:
             lmlist = hands[0]
@@ -79,18 +67,20 @@ def main():
             #this one will center the cat and make it larger
             #if fingerUp == [1, 1, 1, 1, 1]:
                 #make cat larger and center it
-            #     return 0
         
             #this will allow user to pick up the cat
             # if fingerUp == [1, 1, 0, 0, 0]:
-            #     #pick up cat
-            #     return 0
+                #pick up cat
         
             #this will allow the user to close the program
-            if fingerUp == [1, 0, 0, 0, 0]:
-                break
-
-    video.release()
+                if fingerUp == [1, 0, 0, 0, 0]:
+                    cv2.destroyAllWindows()
+                    break
+        if cv2.waitKey(1) & 0xFF == ord('q'): 
+            break
         
+        video.release()
+
+
 if __name__ == '__main__': 
     main()
