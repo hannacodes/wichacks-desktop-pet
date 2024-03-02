25b91e59b7e52e8aa069601fa0a266492932b5b2
ams4222 <ams4222@g.rit.edu>
2024-03-02 15:46:51 -0500

    added sound file

---
+++ sound.py
@@ -0,0 +1,22 @@
+#includes a way to play sound for the cat
+
+from playsound import playsound
+
+def google_sound():
+    playsound('/assets/google.mp3')
+
+def notepad_sound():
+    playsound()
+
+def terminal_sound():
+    playsound()
+
+def settings_sound():
+    playsound()
+
+def throwing():
+    playsound()
+
+#feel free to add more lol
+
+google_sound()
