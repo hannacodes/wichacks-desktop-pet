ec8c4d169714a18046be8c85e28792e9c6ea9640
Hanna Koh <hannakoh12@gmail.com>
2024-03-02 15:06:16 -0500

    throw left and right

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ class pet():
         self.mouse_pressed = False
         self.right = True
         self.down = False
+        self.idling = False
+        self.idlect = 0
         # placeholder image
         self.img = tk.PhotoImage(file="assets/placeholder.png")
 
@@ -44,25 +46,33 @@ class pet():
 
         # run self.update() after 0ms when mainloop starts
         self.window.bind("<Return>", lambda e: self.pick_up())
+        self.window.bind("<l>", lambda e: self.throw_left())
+        self.window.bind("<r>", lambda e: self.throw_right())
         self.update_num = self.window.after(0, self.update)
         self.window.mainloop()
 
     def update(self):
         print(self.x, self.screen_width)
-        if self.right and not self.down:
+        if self.right and not self.down and not self.idling:
             self.x += 1
-            if self.x >= self.screen_width:
+            if self.x >= self.screen_width-100:
+                self.idling = True
                 self.right = False
-        elif not self.down:
+        elif not self.down and not self.idling:
             self.x -= 1
             if self.x <= 0:
+                self.idling
                 self.right = True
-
         if self.down: 
             self.y += 1
             if self.y >= self.screen_height - 165: 
                 self.y = self.screen_height - 165
                 self.down = False
+        if self.idling: 
+            self.idlect += 1
+        if self.idlect > 20:
+            self.idling = False
+
         self.window.geometry('128x128+{x}+{y}'.format(x=str(self.x), y=str(self.y)))
         self.label.configure(image=self.img)
         self.label.pack()
@@ -72,12 +82,32 @@ class pet():
         print("enter")
         self.down = True
         self.window.after_cancel(self.update_num)
-        #self.x = rand.randint(0, self.screen_width)
         self.y = rand.randint(int(self.screen_height/4), 2*int(self.screen_height/4))
         self.window.geometry("128x128+{x}+{y}".format(x=str(self.x), y=str(self.y)))
         self.label.configure(image=self.img)
         self.label.pack()
         self.update_num = self.window.after(10, self.update)
+
+    def throw_left(self):
+        print("left")
+        self.down = True
+        self.window.after_cancel(self.update_num)
+        self.x = 0
+        self.window.geometry("128x128+{x}+{y}".format(x=str(self.x), y=str(self.y)))
+        self.label.configure(image=self.img)
+        self.label.pack()
+        self.update_num = self.window.after(10, self.update)
+
+    def throw_right(self):
+        print("left")
+        self.down = True
+        self.window.after_cancel(self.update_num)
+        self.x = self.screen_width-100
+        self.window.geometry("128x128+{x}+{y}".format(x=str(self.x), y=str(self.y)))
+        self.label.configure(image=self.img)
+        self.label.pack()
+        self.update_num = self.window.after(10, self.update)
+
 
 if __name__ == '__main__':
     pet()
